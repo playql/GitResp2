36757c132e898908b9f476e583e8f1e2f99d84eb
qiaolei <playql@163.com>
2026-06-09 13:43:56 +0800

    添加Student文件

---
+++ Module01/src/com/test/Student.java
@@ -0,0 +1,40 @@
+package com.test;
+
+public class Student {
+    private String name;
+    private int age;
+    private int sex;
+    private int study_no;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public int getStudy_no() {
+        return study_no;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+    public void setStudy_no(int study_no) {
+        this.study_no = study_no;
+    }
+}
